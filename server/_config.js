638ab887d6049be8e8c6712b021dc2a30cb59331
velmurugan <velmurugan@beovolytics.com>
2019-10-22 18:19:45 +0530

    initial commit

---
+++ server/_config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.mongoURI = {
+  development: 'mongodb://176.9.117.149:27098/node-testing',
+  test: 'mongodb://176.9.117.149:27098/node-testing'
+};
+
+module.exports = config;
